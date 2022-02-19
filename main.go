6e69cdb8f1bc78dec4f071210a489eb4dd34294a
Slayzur02 <44528372+Slayzur02@users.noreply.github.com>
2022-02-19 18:52:12 -0500

    business logic of job/internship query-bot

---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/anaskhan96/soup"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	// // creating chromedp bot
+	// ctx, cancel := chromedp.NewContext(context.Background())
+	// defer cancel()
+
+	// ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	// defer cancel()
+
+	// scraping from levels
+	var levelSrc string
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://www.levels.fyi/internships/?track=Software%20Engineer&timeframe=2022%20%2F%202021`),
+		// chromedp.ScrollIntoView(`table`),
+		// chromedp.WaitVisible(`table`),
+		chromedp.OuterHTML(`body`, &levelSrc),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	all_comps := map[string]int{}
+
+	levelCompanies := levelScrape(levelSrc)
+	for _, comp := range levelCompanies {
+		all_comps[comp] = 1
+		continue
+	}
+
+	// scraping from pittsc website
+	resp, err := soup.Get("https://github.com/pittcsc/Summer2022-Internships")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pittCompanies := pittScrape(resp)
+
+	for _, comp := range pittCompanies {
+		if _, ok := all_comps[comp]; ok {
+			continue
+		} else {
+			all_comps[comp] = 1
+		}
+	}
+
+	time.Sleep(1 * time.Second)
+
+	// scrape from discord
+	// var discordLogin string
+	var discordSrc string
+
+	err = chromedp.Run(ctx,
+		chromedp.Navigate("https://discord.com/channels/@me"),
+		// chromedp.OuterHTML(`body`, &discordLogin),
+
+		chromedp.SendKeys(`//input[@name="email"]`, os.Getenv("discord_username"), chromedp.BySearch),
+		chromedp.SendKeys(`//input[@name="password"]`, os.Getenv("discord_password"), chromedp.BySearch),
+		chromedp.Click(`//button[@type="submit"]`, chromedp.BySearch),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(3 * time.Second)
+
+	err = chromedp.Run(ctx,
+		chromedp.Click(`//div[@role="treeitem" and contains(@aria-label, "cscareers.dev")]`, chromedp.BySearch),
+		chromedp.Navigate(`https://discord.com/channels/698366411864670250/856027992701927444`),
+		// chromedp.Navigate("https://discord.com/channels/698366411864670250/856027992701927444"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(4 * time.Second)
+
+	for i := 0; i < 6; i++ {
+		err = chromedp.Run(ctx,
+			chromedp.ScrollIntoView(`//ol/li[1]`, chromedp.BySearch),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	err = chromedp.Run(ctx,
+		chromedp.OuterHTML(`//ol`, &discordSrc, chromedp.BySearch),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discordScrape(discordSrc)
+
+	// turn map into arr
+	allCompSlice := []string{}
+	for comp := range all_comps {
+		allCompSlice = append(allCompSlice, comp)
+	}
+
+	// for _, c := range allCompSlice {
+	// 	fmt.Println(c)
+	// }
+}
+
+func discordScrape(source string) {
+	fmt.Println(source)
+	doc := soup.HTMLParse(source)
+	listItems := doc.FindAll("li")
+
+	fmt.Println(len(listItems))
+	for _, li := range listItems {
+		listAttributes := li.Attrs()
+		if _, ok := listAttributes["id"]; !ok {
+			continue
+		}
+		// has id for list elements
+		text := li.Children()[0].Children()[0].Find("div").Text()
+		if strings.Contains(text, "Please follow") ||
+			strings.Contains(text, "Congrats!!!") ||
+			strings.Contains(text, "Company is not recognized") {
+			continue
+		}
+		fmt.Println(text)
+	}
+}
+
+func levelScrape(source string) (openSpots []string) {
+	doc := soup.HTMLParse(source)
+	rows := doc.Find(`tbody`).FindAll("tr")
+
+	for _, r := range rows {
+		if len(r.Children()) < 4 {
+			continue
+		}
+		comp := r.Find("h6").Text()
+		status := r.Children()[3].Find("a").Text()
+		statusLower := strings.ToLower(status)
+
+		if strings.Contains(statusLower, "apply") {
+			openSpots = append(openSpots, strings.ToLower(strings.Trim(comp, " \n")))
+		}
+	}
+
+	return openSpots
+}
+
+func pittScrape(source string) (openSpots []string) {
+	doc := soup.HTMLParse(source)
+	rows := doc.Find("tbody").FindAll("tr")
+
+	for _, r := range rows {
+		// get the first tag - del or a
+		res := r.Find("td").Children()[0]
+
+		attributes := res.Attrs()
+		if _, ok := attributes["href"]; ok {
+			openSpots = append(openSpots, strings.ToLower(strings.Trim(res.Text(), " ][\n")))
+		}
+	}
+
+	return openSpots
+}
